ade4fe986e917757399159563c015ad0be1fcdfc
philippasigl <philippa@sigl-gloeckner.de>
2018-03-06 09:29:28 +0000

    adding data files

--- views.py
+++ views.py
@@ -8,11 +8,6 @@ from utilities import *
 
 ALLOWED_EXTENSIONS = set(['csv'])
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/', methods = ['GET','POST'])
 def upload_file():        
     UPLOAD_FOLDER ='dummy_data' 
     filename = 'ea_iv_bs.csv'
